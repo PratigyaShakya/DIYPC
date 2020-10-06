de33ba32e0866026dba679420deabd7095e780f7
ShawnUNH <sboma1@unh.newhaven.edu>
2020-10-06 17:58:19 -0400

    Main and Home Activities Separated; Splash PNG Created/Imported

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
