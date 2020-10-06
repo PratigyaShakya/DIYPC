48ac2fb68a84a26cb330ef744e517966bc305b27
Pratigya <pshak1@unh.newhaven.edu>
2020-10-06 18:02:51 -0400

    Gradle sync and dependencies update

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -2,22 +2,23 @@ package com.example.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
-
-class MainActivity : AppCompatActivity() {
-=======
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG = javaClass.name
 
-    private val db = FirebaseFirestore.getInstance()
 
->>>>>>> pratigya-dev
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    class MainActivity : AppCompatActivity() {
+
+        private val TAG = javaClass.name
+
+        private val db = FirebaseFirestore.getInstance()
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        }
     }
 }
