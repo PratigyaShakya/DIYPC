dd85c7cb0b7e3431a3760739acc16585ec560314
Pratigya <pshak1@unh.newhaven.edu>
2020-10-07 00:12:40 -0400

    Working on creating button and new scrolling page created

--- app/src/main/java/com/example/finalproject/FirstLandingActivity.kt
+++ app/src/main/java/com/example/finalproject/FirstLandingActivity.kt
@@ -2,10 +2,14 @@ package com.example.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FirstLandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_landing)
+
+        findViewById<Button>(R.id.button_id)
+
     }
 }
