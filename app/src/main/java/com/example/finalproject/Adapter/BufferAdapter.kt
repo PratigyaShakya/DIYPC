7659e29a46832385e3c6c001d382fa7bdaf65963
Pratigya <pshak1@unh.newhaven.edu>
2020-12-04 20:30:38 -0500

    Continued work on Buffer page. Retrieving data into recycler view. work on pop up ongoing

---
+++ app/src/main/java/com/example/finalproject/Adapter/BufferAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.finalproject.Adapter
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.finalproject.*
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+
+
+class BufferAdapter(options: FirestoreRecyclerOptions<BufferModelActivity>, private val context: Context) :
+    FirestoreRecyclerAdapter<BufferModelActivity, BufferViewHolder>(options) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BufferViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.activity_buffer_viewholder, parent, false)
+        return BufferViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BufferViewHolder, position: Int, model: BufferModelActivity) {
+        // Fires when the user clicks the row in recycler view
+      /* holder.itemView.setOnClickListener {
+            val intent = Intent(context, BufferActivity::class.java).apply {
+                putExtra("Build Show Page", model)
+            }
+            context.startActivity(intent)
+        }*/
+        holder.name.text = model.name
+
+    }
+
+}
--- app/src/main/java/com/example/finalproject/BufferActivity.kt
+++ app/src/main/java/com/example/finalproject/BufferActivity.kt
@@ -1,11 +1,74 @@
 package com.example.finalproject
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.finalproject.Adapter.BufferAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.activity_buffer.*
 
 class BufferActivity: AppCompatActivity() {
+    private val TAG = javaClass.name
+    private val db = FirebaseFirestore.getInstance()
+    private lateinit var bufferAdapter: BufferAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buffer)
+
+        //val buildPage = intent.getSerializableExtra("Build Show Page") as? BufferModelActivity ?: return
+        val query: Query = db
+            .collection("Build")
+            .orderBy("name")
+            .limit(50)
+
+        // query.addSnapshotListener
+
+        val options: FirestoreRecyclerOptions<BufferModelActivity> =
+            FirestoreRecyclerOptions.Builder<BufferModelActivity>()
+            .setQuery(query, BufferModelActivity::class.java)
+            .build()
+
+        //showEditTextDialog()
+        bufferAdapter = BufferAdapter(options, this)
+        recyclerViewBuffer.adapter = bufferAdapter
+        recyclerViewBuffer.layoutManager = LinearLayoutManager(this)
+
+
+    }
+
+    private fun showEditTextDialog() {
+        addButtonBuffer.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            val dialogLayout = inflater.inflate(R.layout.activity_pop_up_buffer, null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.popUpdialogue)
+
+            with(builder) {
+                setPositiveButton("Ok") { dialog, which ->
+                    // need to setup a recycler view first, then this should create a new document in the given
+                    Log.d("Buffer", "Successfully added pop up ")
+                }
+                setNegativeButton("Cancel") { dialog, which ->
+                    Log.d("Buffer", "Successfully added pop up ")
+                }
+                setView(dialogLayout)
+                show()
+            }
+        }
+    }
+
+    override fun onStart(){
+        super.onStart()
+        bufferAdapter.startListening()
+    }
+    override fun onStop(){
+        super.onStop()
+        bufferAdapter.stopListening()
     }
 }
